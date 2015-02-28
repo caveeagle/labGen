3a22efd0b6c08ac0ebe6dce40170beff49ce61e8
caveeagle <a@a.ru>
2015-02-28 15:16:57 +0300

    Точки отдельным массивом, что дальше?

--- LabGen.java
+++ LabGen.java
@@ -76,18 +76,124 @@ public class LabGen
     *   SET labyrinth element
     *   @param X
     *   @param Y
-    *   @param C: block value
+    *   @param C block value
     */
     public void blockSet(int X,int Y, char C)
     { 
         blocks[X][Y] = C;
     } 
+    
 
+    /**
+    *   SET labyrinth element
+    *   @param X точка начала линии
+    *   @param Y точка начала линии
+    *   @param D направление линии ( 1: вверх, 2: вправо, 3:вниз, 4:влево )
+    */
+    public void drawLine(int X,int Y, int D)
+    { 
+        int x0 = X;
+        int y0 = Y;
+
+        int dX = 0;
+        int dY = 0;
+
+        switch (D) {
+            case 1:  dY = -1 ;
+                     break;
+            case 2:  dX = 1 ;
+                     break;
+            case 3:  dY = 1 ;
+                     break;
+            case 4:  dX = -1 ;
+                     break;
+            default: throw new IllegalArgumentException();
+        }        
+        
+        int x1, y1, x2, y2;
+        
+        while(true)
+        {
+            x1 = x0+dX;
+            y1 = y0+dY;
+            
+            x2 = x1+dX;
+            y2 = y1+dY;
+            
+            char ch1 = this.blockGet(x1,y1);
+            
+            if( Character.isWhitespace(ch1) )
+            {
+              this.blockSet(x0,y0,'*');  
+            }
+
+            char ch2 = this.blockGet(x2,y2);
+
+            if( Character.isWhitespace(ch2) && Character.isWhitespace(ch1) )
+            {
+                x0 = x0+dX;
+                y0 = y0+dY;
+            }
+            else
+            {
+                return;
+            }
+        }
+    } 
+    
+    
+    private int[][] pointsInLab;
+    private int pointsInLabNum;
+    
+    private void markOddPoints()
+    {
+        pointsInLab = new int[sizeX][sizeY];
+        pointsInLabNum = 0;
+        
+        /** Clear array */
+        for (int j = 0; j < sizeY; j++)
+        {
+            for (int i = 0; i < sizeX; i++)
+            {
+                pointsInLab[i][j] = 0; // clear array
+            }
+        }
+
+        /** Mark Odd Points */
+        for (int j = 3; j < sizeY-2; j+=2)
+        {
+            for (int i = 3; i < sizeX-2; i+=2)
+            {
+              if( j>16 && j< 25 && i>36 && i<57 ) continue;
+              
+              char ch = this.blockGet(i,j);
+              
+              if( Character.isWhitespace(ch) )
+              { 
+                pointsInLabNum++;
+                pointsInLab[i][j] = pointsInLabNum;  
+              }
+            }
+        }
+
+ 
+    } 
+    
+    public void makeLabyrinth()
+    {
+       this.markOddPoints(); 
+       
+         
+    }
 
      
      public static void main (String[] args)
      {
         LabGen lab = new LabGen(fileIn);
+        
+        lab.makeLabyrinth();
+        
+        //lab.drawLine(46,7,3);
         
         //char bc = lab.blockGet(46,21);
 
